f7df3e1f0c58ce29599f88d4d3777b47d4e7f5b1
Ebrahim <121473338+EbrahimAbdulla@users.noreply.github.com>
2024-05-13 19:30:16 +0530

    minor changes

--- src/main/java/com/retailcloud/employee/RetailCloudApplication.java
+++ src/main/java/com/retailcloud/employee/RetailCloudApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class retailCloudApplication {
+public class RetailCloudApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 	    return new ModelMapper();
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(retailCloudApplication.class, args);
+		SpringApplication.run(RetailCloudApplication.class, args);
 	}
 
 }
